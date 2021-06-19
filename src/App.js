7975f0d54d57f04ccb7c27d55b5a4a4f139e67c7
martinivnv <martinivnv2002@gmail.com>
2021-06-18 22:31:25 -0400

    Add about section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Nav from "./nav.js";
 import Home from "./home.js";
+import About from "./about.js";
 
 function App() {
 	return (
 		<div className="App">
 			<Nav />
 			<Home />
+			<About />
 		</div>
 	);
 }
