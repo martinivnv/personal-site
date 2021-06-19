19c3006b51063e4dda9ff903cd4ebf0f7bef104e
martinivnv <martinivnv2002@gmail.com>
2021-06-18 22:32:55 -0400

    Work on adding images with animated overlay

---
+++ src/image.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./image.css";
+
+const Image = (props) => {
+	const width = props.width;
+	const height = props.height;
+	return <div></div>;
+};
+
+export default Image;
