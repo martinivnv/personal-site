3b5fdae3f3fad681f9a6f41bce2f5c477c19d716
martinivnv <martinivnv2002@gmail.com>
2021-06-20 14:16:44 -0400

    Added social link bar with icons

---
+++ src/SocialLinks.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./SocialLinks.css";
+import { faAt } from "@fortawesome/free-solid-svg-icons";
+import {
+	faGithub,
+	faLinkedinIn,
+	faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const SocialLinks = () => {
+	return (
+		<div class="social-links">
+			<a id="github" class="icon-link" href="https://github.com/martinivnv">
+				<FontAwesomeIcon icon={faGithub} />
+			</a>
+			<a
+				id="linkedin"
+				class="icon-link"
+				href="https://ca.linkedin.com/in/martinivnv"
+			>
+				<FontAwesomeIcon icon={faLinkedinIn} />
+			</a>
+			<a id="email" class="icon-link" href="mailto:martinivnv2002@gmail.com">
+				<FontAwesomeIcon icon={faAt} />
+			</a>
+			<a
+				id="instagram"
+				class="icon-link"
+				href="https://instagram.com/martin.ivnv"
+			>
+				<FontAwesomeIcon icon={faInstagram} />
+			</a>
+		</div>
+	);
+};
+
+export default SocialLinks;
