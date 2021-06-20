966ba3e31eced672bf3552d213c140530c4acea9
martinivnv <martinivnv2002@gmail.com>
2021-06-19 23:58:35 -0400

    Add Projects section

---
+++ src/Projects.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Projects.css";
+
+const Projects = () => {
+	return (
+		<section id="projects">
+			<h2>My Projects</h2>
+		</section>
+	);
+};
+
+export default Projects;
