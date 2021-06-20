27b4a77af858d8de247bae1754ff9a296becc2bf
martinivnv <martinivnv2002@gmail.com>
2021-06-20 02:49:25 -0400

    Add footer section

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Footer.css";
+import Logo from "./resources/logo.png";
+
+const Footer = () => {
+	return (
+		<footer id="footer">
+			<img id="footer-logo" src={Logo} alt="Logo" width="35px" height="35px" />
+			<p id="footer-text">Copyright © Martin Ivanov 2021</p>
+		</footer>
+	);
+};
+
+export default Footer;
