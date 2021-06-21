046ed663052ba211e9a1a0cd01980f4db8d9aed8
martinivnv <martinivnv2002@gmail.com>
2021-06-21 17:37:34 -0400

    Add some of my projects with pictures

---
+++ src/Project.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Project.css";
+import AnimatedImage from "./AnimatedImage.js";
+
+const Project = (props) => {
+	const info = props.info;
+};
+
+export default Project;
--- src/Projects.js
+++ src/Projects.js
@@ -2,9 +2,44 @@ import React from "react";
 import "./Projects.css";
 
 const Projects = () => {
+	const projects = [
+		{
+			name: "simpl_weather",
+			desc: "A bare-bones, ultra minimalist weather web app. Built with a mobile-first approach and fully responsive to different screen sizes.",
+			stack: ["Javascript", "HTML", "CSS", "Express"],
+			github: "https://github.com/martinivnv/simpl-weather",
+			demo: "http://simpl-weather.herokuapp.com/",
+			image: "./resources/screenshots/simpl_weather.png",
+		},
+		{
+			name: "Python Blackjack",
+			desc: "A fully functioning Blackjack game. This project involved painstaking UI design and a heavy amount of Object Oriented Programming principles.",
+			stack: ["Python"],
+			github: "https://github.com/martinivnv/python-blackjack",
+			demo: "",
+			image: "./resources/screenshots/blackjack.png",
+		},
+		{
+			name: "Personal Website",
+			desc: "This is the first thing I have built in React without following a tutorial. Quite happy with how it has turned out!",
+			stack: ["React", "CSS"],
+			github: "https://github.com/martinivnv/personal-site",
+			demo: "#home",
+			image: "./resources/screenshots/personal-site.png",
+		},
+		{
+			name: "DriveTest Cancellation Checker",
+			desc: "A program that sends you an email when someone with a earlier booking than you cancels their driving test.",
+			stack: ["Python"],
+			github: "https://github.com/martinivnv/drivetest-cancellation-checker",
+			demo: "",
+			image: "./resources/screenshots/drivetest-checker.png",
+		},
+	];
 	return (
 		<section id="projects">
 			<h2>Some of My Projects</h2>
+			<div id="projects-container"></div>
 		</section>
 	);
 };
