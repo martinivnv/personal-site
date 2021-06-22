075fef92277f4a5c8195f505c1fdf5096ea412d2
martinivnv <martinivnv2002@gmail.com>
2021-06-22 13:21:13 -0400

    Added extra text

--- src/Contact.js
+++ src/Contact.js
@@ -5,7 +5,11 @@ import ContactForm from "./ContactForm";
 const Contact = () => {
 	return (
 		<section id="contact">
-			<h2>Thanks for dropping by. Let's talk!</h2>
+			<h2 id="contact-title">Thanks for dropping by. Let's talk!</h2>
+			<p id="contact-text">
+				Want to discuss a project, ask a question, or just say hi? Leave me a
+				message and I'll make sure to get back to you.
+			</p>
 			<ContactForm />
 		</section>
 	);
