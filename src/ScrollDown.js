d2aa3a397f7f162f23dacb33efda27929fdfb9b4
martinivnv <martinivnv2002@gmail.com>
2021-06-22 15:53:04 -0400

    Added scrolldown icon for mobile

---
+++ src/ScrollDown.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./ScrollDown.css";
+
+/*Credit: https://codepen.io/xonic/pen/KWMaqe?editors=1100*/
+
+const ScrollDown = () => {
+	return (
+		<div className="scrolldown-container">
+			<div className="chevron"></div>
+			<div className="chevron"></div>
+			<div className="chevron"></div>
+		</div>
+	);
+};
+
+export default ScrollDown;
