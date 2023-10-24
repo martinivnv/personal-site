aa9fd7cc82652d4190468692957c8eff62426df7
martinivnv <martinivnv2002@gmail.com>
2023-10-24 01:12:02 -0400

    Revert "Add new animated background"
    This reverts commit 6b4f5ff22b706f9448566958968ea9cd81cadc2f.

--- src/Home.js
+++ src/Home.js
@@ -1,31 +1,22 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "./Home.css";
+import myVideoV1 from "./resources/grid background.mp4";
+import myVideoV2 from "./resources/grid background.ogg";
 import ScrollDown from "./ScrollDown";
 import SocialLinks from "./SocialLinks";
 import Typist from "react-typist";
-import * as p5 from "p5";
-import TRUNK from "vanta/src/vanta.trunk";
 
 const Home = () => {
-	useEffect(() => {
-		TRUNK({
-			el: "#background-js",
-			mouseControls: false,
-			touchControls: false,
-			gyroControls: false,
-			scale: 1.0,
-			scaleMobile: 1.0,
-			color: 0x3500d3,
-			backgroundColor: 0x000000,
-			spacing: 10.0,
-			chaos: 10.0,
-			p5: p5,
-		});
-	});
-
 	return (
 		<div id="home">
-			<div id="background-js"></div>
+			<video autoPlay muted loop id="my-video">
+				<source src={myVideoV1} type="video/mp4" />
+				Your browser does not support the video tag. I suggest you upgrade your
+				browser.
+				<source src={myVideoV2} type="video/ogg" />
+				Your browser does not support the video tag. I suggest you upgrade your
+				browser.
+			</video>
 			<div className="content">
 				<Typist cursor={{ show: false }} avgTypingDelay={80}>
 					<h1 id="name">Hey, I'm Martin.</h1>
